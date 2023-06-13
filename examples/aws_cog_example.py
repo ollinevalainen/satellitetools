926631cef179bfb996ae6c7f5bc11ab8ac6e7df5
Olli Nevalainen <olli.nevalainen.fmi@gmail.com>
2023-06-13 12:46:21 +0300

    Adapted AWS source to Earth serach v1, older v0 deprecated

--- examples/aws_cog_example.py
+++ examples/aws_cog_example.py
@@ -7,7 +7,7 @@ Created on Fri Mar 12 16:20:30 2021
 """
 
 
-#%%
+# %%
 import satellitetools.aws_cog as aws_cog
 from satellitetools.common.classes import AOI, RequestParams
 from satellitetools.common.sentinel2 import S2_BANDS_10_20_COG
@@ -16,7 +16,7 @@ import time
 
 start_time = time.time()
 ruukki_df = gpd.read_file(
-    "/Users/olli/python/satellitetools/geometry-files/ruukki_blocks_wgs84.shp"
+    "/data/field-observatory/geometry-files/ruukki_blocks_wgs84.shp"
 )
 ruukki_block1 = ruukki_df.loc[[0]]
 aoi_name = ruukki_block1.iloc[0].Name.replace(" ", "").lower()
@@ -25,10 +25,12 @@ crs = ruukki_block1.crs.to_string()
 
 aoi = AOI(aoi_name, geom, crs)
 req_params = RequestParams(
-    "2020-06-01", "2020-06-10", "aws_cog", bands=S2_BANDS_10_20_COG, target_gsd=10.0
+    "2023-06-01", "2023-06-10", "aws_cog", bands=S2_BANDS_10_20_COG, target_gsd=10.0
 )
-#%%
+# %%
 items = aws_cog.search_s2_cogs(aoi, req_params)
+
+# %%
 qi_df = aws_cog.cog_get_s2_quality_info(aoi, req_params, items)
 dataset = aws_cog.cog_get_s2_band_data(aoi, req_params, items, qi_df)
 
@@ -38,7 +40,7 @@ aoi.data = dataset
 end_time = time.time()
 
 print(end_time - start_time)
-#%% Alternative using wrapper
+# %% Alternative using wrapper
 from satellitetools.common.wrappers import get_s2_qi_and_data
 
 datasource = "aws_cog"
@@ -53,7 +55,7 @@ request = RequestParams(
 )
 aoi.qi, aoi.data = get_s2_qi_and_data(aoi, request, qi_threshold=qi_threshold)
 
-#%% Test biohys
+# %% Test biohys
 import satellitetools.biophys as biophys
 
 aoi.data = biophys.run_snap_biophys(aoi.data, "LAI")
