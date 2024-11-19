c74e92651265f63f1e05720eba0c769833b8f52f
Olli Nevalainen <olli.nevalainen.fmi@gmail.com>
2024-11-19 19:31:54 +0200

    Run tests for python 3.12 with nox

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
 import nox
 
+home = Path.home()
+nox.options.envdir = home / ".cache/.nox"
 
-@nox.session(python=["3.10", "3.11"])
+
+@nox.session(python=["3.10", "3.11", "3.12"])
 def test(session):
     session.install(".")
     session.install("pytest")
